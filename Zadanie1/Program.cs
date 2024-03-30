98d252e9f4ee441b6e029dd39f695318a670b3b6
banika13 <44238236+banika13@users.noreply.github.com>
2024-03-30 13:50:41 +0500

    Add files via upload

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,54 @@
+﻿using MySqlConnector;
+
+namespace Zadanie1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FIO> fioS = new List<FIO>();  
+            var builder = new MySqlConnectionStringBuilder
+            {
+                Server = "localhost",
+                Database = "sys",
+                UserID = "Admin",
+                Password = "Admin",
+                SslMode = MySqlSslMode.Required,
+            };
+
+            using (var conn = new MySqlConnection(builder.ConnectionString))
+            {
+                Console.WriteLine("Opening connection");
+                conn.Open();
+                using (var command = conn.CreateCommand())
+                {
+                   
+                    command.CommandText = "SELECT * FROM fio WHERE age < @age";
+                    int age = 40;
+                    command.Parameters.AddWithValue("@age",age);
+                    using (var reader = command.ExecuteReader())
+                    {
+                       
+                        while (reader.Read())
+                        {
+                              fioS.Add(new FIO { Id = reader.GetInt32(0),LastName =  reader.GetString(1),
+                                  FirstName = reader.GetString(2),Age = reader.GetInt32(3) });
+                        }
+                    }
+                }
+                    
+             
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"ID|LastName|FirstName|Age");
+            Console.ResetColor();   
+            foreach (var s in fioS)
+            {
+                Console.WriteLine($"{s.Id}|{s.LastName}|{s.FirstName}|{s.Age}");
+            }
+
+
+
+        }
+    }
+}
